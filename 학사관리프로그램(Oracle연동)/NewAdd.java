10b88c02cc7fac76255f5b3c5b70f84ba2fe8aca
SimDaeSung1 <86938974+ShimDaeSung1@users.noreply.github.com>
2022-03-17 20:52:11 +0900

    Update NewAdd.java

--- 학사관리프로그램(Oracle연동)/NewAdd.java
+++ 학사관리프로그램(Oracle연동)/NewAdd.java
@@ -24,7 +24,7 @@ public class NewAdd extends JFrame {
 		setTitle("회원가입");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(460,470);
-		setLocationRelativeTo(null);
+// 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5,5,5,5));
 		setContentPane(contentPane);
